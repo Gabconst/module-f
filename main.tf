9ce12d4bd8890063bcfbcd5c0d060a1e5e425993
Gabriel Correa Pereira <gabriel.pereira@nikos.com.br>
2025-08-22 17:16:57 -0300

    primeiro modulo

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+module "grafana" {
+  source = "git::https://github.com/Gabconst/GRAFANA-IAC.git//compute?ref=main"
+
+  ami_id               = "ami-020cba7c55df1f615"
+  instance_type        = "t2.micro"
+  key_name             = "devops-pdi"
+  allowed_ssh_cidr     = "0.0.0.0/0"
+  allowed_grafana_cidr = "0.0.0.0/0"
+  aws_profile          = "default"
+  aws_region           = "us-east-1"
+}
+
+output "instance_ip" {
+  value = module.grafana.instance_ip
+}
+
+output "security_group_id" {
+  value = module.grafana.security_group_id
+}
